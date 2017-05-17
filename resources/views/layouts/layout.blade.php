e0b50acf43f4fbb6da28633b7558d295f3ce4c92
Leiviton Carlos <leivitoncs@gmail.com>
2017-05-17 17:38:54 -0300

    player

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -35,7 +35,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.0.3.min.js"></script>
     <script type="text/javascript">
         diretorio = "/theme/img/sound/";
         musicas = ["1"];
